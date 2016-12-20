adf906a5ffba9e6672da749c022795108f1bbd4e
kevinw123 <kevinwong_895@hotmail.com>
2016-12-19 22:50:13 -0800

    Adding next page button

--- js/script.js
+++ js/script.js
@@ -1,64 +1,86 @@
-$(function(){
-	var searchField = $('#query');
+$(function() {
+    var searchField = $('#query');
 
-	$('#search-form').submit(function(e){
-		e.preventDefault();
-	});
+    $('#search-form').submit(function(e) {
+        e.preventDefault();
+    });
 })
-function search(){
-	// Clear Results
-	$('#results').html('');
-	$('#buttons').html('');
 
-	// Get Form Input
-	q = $('#query').val();
+function search() {
+    // Clear Results
+    $('#results').html('');
+    $('#buttons').html('');
 
-	// Run GET Request on API
-	$.get(
-		"https://www.googleapis.com/youtube/v3/search",{
-			part: 'snippet, id',
-			q: q,
-			type: 'video',
-			key: 'AIzaSyDjt9iez6HTzkKgk9aQ7z8BUA10nmNNgRg'},
-			function(data){
-				var nextPageToken = data.nextPageToken;
-				var prevPageToken = data.prevPageToken;
+    // Get Form Input
+    q = $('#query').val();
 
-				// Log data
-				console.log(data);
-				$.each(data.items, function(i, item){
-					// GetOutput
-					var output = getOutput(item);
+    // Run GET Request on API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
+            part: 'snippet, id',
+            q: q,
+            type: 'video',
+            key: 'AIzaSyDjt9iez6HTzkKgk9aQ7z8BUA10nmNNgRg'
+        },
+        function(data) {
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
 
-					// Display Results
-					$('#results').append(output);
-				});
-			}
-	);
+            $.each(data.items, function(i, item) {
+                // GetOutput
+                var output = getOutput(item);
+
+                // Display Results
+                $('#results').append(output);
+            });
+
+            var buttons = getButtons(prevPageToken, nextPageToken);
+
+            // Display Buttons
+            $('#buttons').append(buttons);
+        }
+    );
 }
 
 // Build Output
-function getOutput(item){
-	var videoId = item.id.videoId;
-	var title = item.snippet.title;
-	var description = item.snippet.description;
-	var thumb = item.snippet.thumbnails.high.url;
-	var channelTitle = item.snippet.channelTitle;
-	var videoDate = item.snippet.publishedAt;
+function getOutput(item) {
+    var videoId = item.id.videoId;
+    var title = item.snippet.title;
+    var description = item.snippet.description;
+    var thumb = item.snippet.thumbnails.high.url;
+    var channelTitle = item.snippet.channelTitle;
+    var videoDate = item.snippet.publishedAt;
 
-	// Build Output String
-	var output = '<li>' + 
-	'<div class="list-left">' + 
-	'<img src="' + thumb +'">' + 
-	'</div>' +
-	'<div class="list-right">' +
-	'<h3>' + title + '</h3>' + 
-	'<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + '</small>' +
-	'<p>' + description + '</p>' + 
-	'</div>' + 
-	'</li>' +
-	'<div class="clearfix"></div>' +
-	'';
+    // Build Output String
+    var output = '<li>' +
+        '<div class="list-left">' +
+        '<img src="' + thumb + '">' +
+        '</div>' +
+        '<div class="list-right">' +
+        '<h3>' + title + '</h3>' +
+        '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + '</small>' +
+        '<p>' + description + '</p>' +
+        '</div>' +
+        '</li>' +
+        '<div class="clearfix"></div>' +
+        '';
 
-	return output;
+    return output;
+}
+
+// Build the buttons
+function getButtons(prevPageToken, nextPageToken) {
+    if (!prevPageToken) {
+        var btnoutput = '<div class="button-container">' +
+            '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+            'onclick="nextPage();">Next Page</button></div>';
+    } else {
+        var btnoutput = '<div class="button-container">' +
+            '<button id="next-button" class="paging-button" data-token="' + prevPageToken + '" data-query="' + q + '"' +
+            'onclick="prevPage();">Previous Page</button>' +
+            '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+            'onclick="nextPage();">Next Page</button></div>';
+    }
+
+    return btnoutput;
 }
